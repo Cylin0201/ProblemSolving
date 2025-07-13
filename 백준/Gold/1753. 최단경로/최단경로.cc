349501570b2f82b4966d170428a0a54563d54237
Cylin0201 <138640770+Cylin0201@users.noreply.github.com>
2025-07-13 17:43:21 +0900

    [Gold IV] Title: 최단경로, Time: 128 ms, Memory: 9192 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+
+std::vector<std::vector<std::pair<int, int>>> a;
+std::vector<int> d;
+
+void dijkstra(int start) {
+    d[start] = 0;
+    std::priority_queue<std::pair<int, int>,
+        std::vector<std::pair<int, int>>,
+        std::greater<>> pq;
+
+    pq.push({ 0, start });  // 시작 노드: 거리 0
+
+    while (!pq.empty()) {
+        int distance = pq.top().first;
+        int current = pq.top().second;
+        pq.pop();
+
+        if (d[current] < distance) continue;
+
+        for (const auto& edge : a[current]) {
+            int next = edge.first;
+            int nextDistance = distance + edge.second;
+
+            if (nextDistance < d[next]) {
+                d[next] = nextDistance;
+                pq.push({ nextDistance, next });
+            }
+        }
+    }
+}
+
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    int V, E;
+    std::cin >> V >> E;
+
+    int start;
+    std::cin >> start;
+
+    a.assign(V + 1, std::vector<std::pair<int, int>>());
+    d.assign(V + 1, INF);
+
+    for (int i = 0; i < E; i++) {
+        int x, y, w;
+        std::cin >> x >> y >> w;
+        a[x].push_back({ y, w });
+    }
+
+    dijkstra(start);
+
+    for (int i = 1; i <= V; i++) {
+        if (d[i] != INF)
+            std::cout << d[i] << '\n';
+        else
+            std::cout << "INF\n";
+    }
+}
