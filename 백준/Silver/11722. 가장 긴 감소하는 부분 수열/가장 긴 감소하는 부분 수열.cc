4a898eb5a67cd171dbb5ccc2d225a1c418dd3754
Cylin0201 <138640770+Cylin0201@users.noreply.github.com>
2025-07-24 16:17:59 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+int main() {
+    int N; std::cin >> N;
+    std::vector<int> A(N), dp(N, 1);
+
+    for (int i = 0; i < N; i++) std::cin >> A[i];
+
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            if (A[j] > A[i]) {
+                dp[i] = std::max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    std::cout << *std::max_element(dp.begin(), dp.end()) << '\n';
+}
