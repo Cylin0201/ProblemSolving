d9215dcd4e840f38fac8291d1bc3f39597a1ec07
Cylin0201 <cylin0201@naver.com>
2025-11-06 17:23:01 +0900

    [Gold IV] Title: 비밀 모임, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/13424. 비밀 모임/비밀 모임.cc
+++ 백준/Gold/13424. 비밀 모임/비밀 모임.cc
@@ -1,59 +1,60 @@
 #include <bits/stdc++.h>
 
-int N, M, K;
-std::vector<std::vector<std::pair<int, int>>> graph;
-std::vector<int> dist, sumDist;
-
-void dijkstra(int start) {
-    dist.assign(N + 1, 1e9);
-    dist[start] = 0;
-
-    std::priority_queue<std::pair<int, int>> pq;
-    pq.push({ 0, start });
-
-    while (!pq.empty()) {
-        int cur = pq.top().second, distance = -pq.top().first;
-        pq.pop();
-
-        if (dist[cur] < distance) continue;
-
-        for (const auto& elem : graph[cur]) {
-            int next = elem.second, nextDistance = elem.first + distance;
-
-            if (dist[next] > nextDistance) {
-                dist[next] = nextDistance;
-                pq.push({ -nextDistance, next });
-            }
-        }
-    }
-}
-
 int main() {
     int T; std::cin >> T;
-
+    
     while (T-- > 0) {
-        std::cin >> N >> M;
-        graph.assign(N + 1, {});
+        int N, M, K; std::cin >> N >> M;
+        std::vector<std::vector<int>> dist(N + 1, std::vector<int>(N + 1, 1e9));
+
+        for (int i = 1; i <= N; i++)
+            dist[i][i] = 0;
+
         for (int i = 0; i < M; i++) {
             int a, b, d; std::cin >> a >> b >> d;
-            graph[a].push_back({ d, b });
-            graph[b].push_back({ d, a });
+            dist[a][b] = d;
+            dist[b][a] = d;
         }
         std::cin >> K;
-        sumDist.assign(N + 1, 0);
+        std::vector<int> starts(K);
         for (int i = 0; i < K; i++) {
-            int start; std::cin >> start;
-            dijkstra(start);
-            for (int j = 1; j <= N; j++)
-                sumDist[j] += dist[j];
+            std::cin >> starts[i];
         }
 
-        int minIdx = 1;
-        for (int i = 2; i <= N; i++) {
-            if (sumDist[i] < sumDist[minIdx]) minIdx = i;
+        std::vector<std::pair<int, int>> sumDist(N + 1, { 0, 0 });
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (i == j) continue;
+                    dist[i][j] = std::min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
         }
+
+        for (const auto& s : starts) {
+            for (int i = 1; i <= N; i++) {
+                sumDist[i].first++;
+                sumDist[i].second += dist[s][i];
+            }
+        }
+
+        int startIdx = 1;
+        for (int i = 1; i <= N; i++) {
+            if (sumDist[i].first == K) {
+                startIdx = i;
+                break;
+            }
+        }
+
+        int minIdx = startIdx;
+
+        for (int i = startIdx + 1; i <= N; i++) {
+            if (sumDist[i].first == K && sumDist[i].second < sumDist[minIdx].second)
+                minIdx = i;
+        }
+
         std::cout << minIdx << '\n';
     }
-
     return 0;
 }
